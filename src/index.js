14e200bc0c2a953e4bf3b1c968f3bf8c614e3028
Gitlayzer <18037803502@163.com>
2024-10-16 09:12:19 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,19 +5,21 @@ addEventListener("fetch", (event) => {
 
 const dockerHub = "https://registry-1.docker.io";
 
+const DOMAIN = "cloudimages.asia"
+
 const routes = {
   // production
-  ["docker." + CUSTOM_DOMAIN]: dockerHub,
-  ["quay." + CUSTOM_DOMAIN]: "https://quay.io",
-  ["gcr." + CUSTOM_DOMAIN]: "https://gcr.io",
-  ["k8s-gcr." + CUSTOM_DOMAIN]: "https://k8s.gcr.io",
-  ["k8s." + CUSTOM_DOMAIN]: "https://registry.k8s.io",
-  ["ghcr." + CUSTOM_DOMAIN]: "https://ghcr.io",
-  ["cloudsmith." + CUSTOM_DOMAIN]: "https://docker.cloudsmith.io",
-  ["ecr." + CUSTOM_DOMAIN]: "https://public.ecr.aws",
+  ["docker." + DOMAIN]: dockerHub,
+  ["quay." + DOMAIN]: "https://quay.io",
+  ["gcr." + DOMAIN]: "https://gcr.io",
+  ["k8s-gcr." + DOMAIN]: "https://k8s.gcr.io",
+  ["k8s." + DOMAIN]: "https://registry.k8s.io",
+  ["ghcr." + DOMAIN]: "https://ghcr.io",
+  ["cloudsmith." + DOMAIN]: "https://docker.cloudsmith.io",
+  ["ecr." + DOMAIN]: "https://public.ecr.aws",
 
   // staging
-  ["docker-staging." + CUSTOM_DOMAIN]: dockerHub,
+  ["docker-staging." + DOMAIN]: dockerHub,
 };
 
 function routeByHosts(host) {
